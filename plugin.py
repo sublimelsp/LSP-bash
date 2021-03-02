1033c58f40fa3d84fd8c50b4f62b906fb20d9b32
Rafał Chłodnicki <rchl2k@gmail.com>
2021-03-02 09:26:46 +0100

    Use ${node_bin} for the node binary (#16)

--- plugin.py
+++ plugin.py
@@ -1,5 +1,5 @@
-import os
 from lsp_utils import NpmClientHandler
+import os
 
 
 def plugin_loaded():
@@ -13,14 +13,4 @@ def plugin_unloaded():
 class LspBashPlugin(NpmClientHandler):
     package_name = __package__
     server_directory = 'language-server'
-    server_binary_path = os.path.join(
-        server_directory, 'node_modules', 'bash-language-server', 'bin', 'main.js'
-    )
-
-    @classmethod
-    def get_binary_arguments(cls):
-        return ['start']
-
-    @classmethod
-    def install_in_cache(cls) -> bool:
-        return False
+    server_binary_path = os.path.join(server_directory, 'node_modules', 'bash-language-server', 'bin', 'main.js')
